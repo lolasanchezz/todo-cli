2c1d871c84b3043e4ca61ef20aab6dafaeee8f24
lolasanchezz <lolansanchez@icloud.com>
2025-03-09 22:03:26 -0400

    first version of cli

--- todo.go
+++ todo.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"log"
 	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 type todoItem struct {
@@ -21,14 +23,18 @@ type model struct {
 
 func main() {
 	fmt.Print("hii")
-	initialModel()
-
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
 
 func initialModel() model {
 
 	return model{
-		todos: readFromFile(),
+		todos:    readFromFile(),
+		selected: make(map[int]struct{}),
 	}
 
 }
@@ -46,8 +52,55 @@ func readFromFile() []todoItem {
 	return data
 }
 
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
 func er(Error error) {
 	if Error != nil {
 		log.Fatal(Error)
 	}
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down":
+			if m.cursor < len(m.todos) {
+				m.cursor++
+			}
+		}
+
+	}
+	return m, nil
+
+}
+
+func (m model) View() string {
+	s := "todo \n" //hopefully we add todo page name here soon!
+	//need another struct for all the placeholder values
+
+	for i, todo := range m.todos {
+		cursor := ""
+		completed := ""
+		if i == m.cursor {
+			cursor = ">"
+		}
+		if todo.Completed {
+			completed = "X"
+		}
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, completed, todo.Name)
+	}
+	return s
+
 }
