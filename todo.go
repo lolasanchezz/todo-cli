3482f15d61708bf9608a65a714f9559cbfb1e122
lolasanchezz <lolansanchez@icloud.com>
2025-03-09 21:37:01 -0400

    file reading finally works

--- todo.go
+++ todo.go
@@ -1,9 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
+
+type todoItem struct {
+	Name      string
+	Completed bool
+}
+
+type model struct {
+	todos    []todoItem
+	cursor   int
+	selected map[int]struct{}
+}
 
 func main() {
 	fmt.Print("hii")
+	initialModel()
+
+}
+
+func initialModel() model {
+
+	return model{
+		todos: readFromFile(),
+	}
+
+}
+
+func readFromFile() []todoItem {
+	file, err := os.OpenFile("tasks.json", os.O_APPEND|os.O_CREATE|os.O_RDONLY, 0644)
+	er(err)
+	defer file.Close()
+	fileCont, err := io.ReadAll(file)
+	er(err)
+	var data []todoItem
+	err = json.Unmarshal(fileCont, &data)
+	er(err)
+
+	return data
+}
+
+func er(Error error) {
+	if Error != nil {
+		log.Fatal(Error)
+	}
 }
