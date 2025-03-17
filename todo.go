8bb812174ad00ddc9e22ffe4c72262ba3da8dee6
lolasanchezz <lolansanchez@icloud.com>
2025-03-16 22:24:22 -0400

    um. pagination kinda working. rly buggy but just a progress check

--- todo.go
+++ todo.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"slices"
 
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
@@ -13,15 +14,20 @@ import (
 )
 
 type todoItem struct {
-	Name      string
-	Completed bool
+	Name      string `json:"Name"`
+	Completed bool   `json:"Completed"`
 }
-
+type allTodos struct {
+	PageName string
+	Todos    []todoItem
+}
 type model struct {
-	todos     []todoItem
-	cursor    int
-	selected  map[int]struct{}
-	textInput textinput.Model
+	todos           []allTodos
+	cursor          int
+	pageNum         int
+	selected        allTodos
+	textInput       textinput.Model
+	currentPageName textinput.Model
 }
 
 var style = lipgloss.NewStyle().
@@ -30,8 +36,8 @@ var style = lipgloss.NewStyle().
 	//Background(lipgloss.Color("#7D56F4")).
 	PaddingTop(2).
 	PaddingLeft(4).
-	Width(22).
-	Align(lipgloss.Center)
+	//Width(22)
+	Align(lipgloss.Left)
 
 func main() {
 
@@ -42,7 +48,6 @@ func main() {
 	}
 	defer f.Close()
 
-	fmt.Print("hii")
 	p := tea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
@@ -52,30 +57,49 @@ func main() {
 
 func initialModel() model {
 	ti := textinput.New()
+	title := textinput.New()
 	ti.Placeholder = "new todo"
 	ti.Focus()
 	ti.CharLimit = 156
 	ti.Width = 20
+	initData := readFromFile()
+	title.Placeholder = initData[0].PageName
+
+	title.CharLimit = 156
+	title.Width = 20
 	return model{
-		todos:     readFromFile(),
-		selected:  make(map[int]struct{}),
-		cursor:    0,
-		textInput: ti,
+		todos:           initData,
+		pageNum:         0,
+		selected:        initData[0],
+		cursor:          0,
+		textInput:       ti,
+		currentPageName: title,
 	}
 
 }
 
-func readFromFile() []todoItem {
+func readFromFile() []allTodos {
 	file, err := os.OpenFile("tasks.json", os.O_APPEND|os.O_CREATE|os.O_RDONLY, 0644)
 	er(err)
 	defer file.Close()
 	fileCont, err := io.ReadAll(file)
 	er(err)
-	var data []todoItem
+	var data []allTodos
 	err = json.Unmarshal(fileCont, &data)
 	er(err)
 
 	return data
+}
+
+func writeToFile(todos []allTodos) {
+
+	//turn into json string
+	data, err := json.Marshal(todos)
+
+	er(err)
+	err = os.WriteFile("tasks.json", data, os.FileMode(os.O_WRONLY))
+
+	er(err)
 }
 
 func (m model) Init() tea.Cmd {
@@ -96,43 +120,102 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.KeyMsg:
 
 		switch msg.String() {
-		case "ctrl+c", "esc":
-			return m, tea.Quit
-		case "up":
-			if m.cursor > 0 {
-				m.cursor--
+		case "right":
+			if m.pageNum < len(m.todos)-1 {
+				m.pageNum += 1
+				m.selected = m.todos[m.pageNum]
+				m.currentPageName.SetValue(m.todos[m.pageNum].PageName)
+
+			} else if m.pageNum == len(m.todos)-1 {
+				m.pageNum = 0
+				m.selected = m.todos[m.pageNum]
+				m.currentPageName.SetValue(m.todos[m.pageNum].PageName)
+
 			}
+
+		case "left":
+			if m.pageNum > 0 {
+				m.pageNum -= 1
+				m.selected = m.todos[m.pageNum]
+				m.currentPageName.SetValue(m.todos[m.pageNum].PageName)
+			} else if m.pageNum == 0 {
+				m.pageNum = len(m.todos) - 1
+				m.selected = m.todos[m.pageNum]
+				m.currentPageName.SetValue(m.todos[m.pageNum].PageName)
+
+			}
+		case "ctrl+c", "esc":
+			if m.cursor != len(m.selected.Todos) {
+				return m, tea.Quit
+			}
+		case "q":
+			if m.cursor != len(m.selected.Todos) || m.cursor != -1 {
+				return m, tea.Quit
+			}
+		case "up":
+			if m.cursor > -1 {
+				m.cursor--
+				fmt.Print(m.cursor)
+
+			}
+			if m.cursor == -1 {
+				m.currentPageName.Focus()
+			}
+
 		case "down":
-			if m.cursor < len(m.todos) {
+			if m.cursor < len(m.selected.Todos) {
 				m.cursor++
+				m.currentPageName.Blur()
 			}
 		case "enter":
-			if m.cursor == len(m.todos) {
-				m.todos = append(m.todos, todoItem{Name: m.textInput.Value()})
+			if m.cursor == len(m.selected.Todos) {
+				m.selected.Todos = append(m.selected.Todos, todoItem{
+					Name: m.textInput.Value(),
+				})
+				m.todos = append(m.todos, m.selected)
+				m.textInput.Reset()
+				m.todos[m.cursor] = m.selected
+			} else if m.cursor > 0 {
+				m.selected.Todos[m.cursor].Completed = !(m.selected.Todos[m.cursor].Completed)
+				m.todos[m.cursor] = m.selected
+			} else if m.cursor == -1 {
+				m.selected.PageName = m.currentPageName.Value()
+				m.currentPageName.SetValue(m.selected.PageName)
+
 			}
-			m.todos[m.cursor].Completed = !(m.todos[m.cursor].Completed)
 		case "backspace":
 			//m.todos[m.cursor].Name = "hello"
-			if m.cursor < len(m.todos) {
-				m.todos[m.cursor] = m.todos[len(m.todos)-1]
-				m.todos = append(m.todos[:m.cursor], m.todos[m.cursor+1:]...)
+			//make SURE that the cursor is on a todo item
+			if m.cursor < len(m.selected.Todos) && m.cursor != -1 {
+				//we must reslice
+
+				m.selected.Todos = slices.Delete(m.selected.Todos, m.cursor, m.cursor+1)
+				//run the below everytime the todos are updates
+				m.todos[m.pageNum] = m.selected
+				//i hope this works...
 			}
+
 		}
 
 	}
-	if m.cursor == len(m.todos) {
+	if m.cursor == len(m.selected.Todos) {
 		m.textInput, _ = m.textInput.Update(msg)
 	}
+	if m.cursor == -1 {
+		m.currentPageName, _ = m.currentPageName.Update(msg)
+		m.todos[m.pageNum].PageName = m.currentPageName.Value()
+	}
 
+	defer writeToFile(m.todos)
 	return m, nil
 
 }
 
 func (m model) View() string {
-	s := "todo \n" //hopefully we add todo page name here soon!
+	s := fmt.Sprintf("%s \n", m.currentPageName.View()) //hopefully we add todo page name here soon!
 	//need another struct for all the placeholder values
-
-	for i, todo := range m.todos {
+	m.currentPageName.SetValue(m.todos[0].PageName)
+	for i, todo := range m.selected.Todos {
 		cursor := ""
 		completed := ""
 		if i == m.cursor {
@@ -143,7 +226,7 @@ func (m model) View() string {
 		}
 		s += fmt.Sprintf("%s [%s] %s \n", cursor, completed, todo.Name)
 	}
-	if m.cursor == len(m.todos) {
+	if m.cursor == len(m.selected.Todos) {
 		s += fmt.Sprintf(m.textInput.View())
 	}
 	s = style.Render(s)
